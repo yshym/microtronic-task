11f56da4709df768f5b362b1cbb307b4b05ad9b6
fly1ngDream <eugen228228@gmail.com>
2019-04-26 23:24:34 +0300

    Coords list fetching was optimized

--- django/main/views.py
+++ django/main/views.py
@@ -4,22 +4,15 @@ from django.conf import settings
 from .models import Area
 
 
+def tuple2list(ctuple):
+    return list((tuple2list(elt) if isinstance(elt, tuple) else elt for elt in ctuple))
+
+
 class HomeView(TemplateView):
     template_name = 'home.djhtml'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['GOOGLE_MAPS_API_KEY'] = settings.GOOGLE_MAPS_API_KEY
-        coords_list = []
-        for area in Area.objects.all():
-            current_coords = area.polygon.coords
-            current_coords = list(current_coords)
-            current_coords[0] = list(current_coords[0])
-
-            for index, coord in enumerate(current_coords[0]):
-                current_coords[0][index] = list(coord)
-
-            coords_list.append(current_coords)
-
-        context['coords_list'] = coords_list
+        context['coords_list'] = [tuple2list(area.polygon.coords) for area in Area.objects.all()]
         return context
